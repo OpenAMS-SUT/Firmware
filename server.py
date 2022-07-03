fa860887feff0fcee24c40a8bd3563aeca8e4885
NikodemBartnik <nikodem.bartnik@gmail.com>
2022-07-03 15:08:59 +0200

    Panel

--- server.py
+++ server.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/panel")
 def pageMain():
-    return render_template('test.html')
+    return render_template('panel.html')
 
+@app.route("/info")
+def pageInfo():
+    return render_template('info.html')
+
+@app.route("/settings")
+def pageSettings():
+    return render_template('settings.html')
 
 app.run(host='0.0.0.0', port=80, debug=True)
