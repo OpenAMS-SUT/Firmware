006f6e00e0d2d955f56606816874c389f8c7334f
NikodemBartnik <nikodem.bartnik@gmail.com>
2022-08-02 11:43:27 +0200

    state fix, layout fix

--- server.py
+++ server.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, request
 #import stepper
 
 app = Flask(__name__)
-state = True
+# state description:
+# 0: error
+# 1: nominal, ready to accept commands
+# 2: moving, wait until ready
+state = 1
 #stepper = stepper.MotorDriver
 
 @app.route("/")
@@ -25,17 +29,22 @@ def moveSteppers():
     global state
     angle = request.args.get('angle')
     print(angle)
-    state = False
+    state = 2
     #move motors here
     #stepper.move()
     sleep(int(angle)) #just for testing
-    state = True
+    state = 1
     return "ok"
 
 @app.route("/_checkState")
 def checkState():
     #can be improved to show more states than just two, maybe even error codes
     return str(state)
+
+@app.route("/_emergencyStop")
+def emergencyStop():
+    #implement emergency stop
+    pass
 
 
 app.run(host='0.0.0.0', port=80, debug=True)
--- static/js/custom.js
+++ static/js/custom.js
@@ -5,7 +5,9 @@ function autoModeClick() {
         <label for="measurements1" class="form-label">Liczba pomiarów</label>
         <input type="number" class="form-control" id="measurements1" aria-describedby="measurements1Help" min="2" max="100">
         <div id="measurements1Help" class="form-text">Pomiary będą rozmieszczone równomiernie na okręgu.
-        Przedział 2-100</div>`;
+        Przedział 2-100</div>
+        <button type="button" class="btn btn-primary" id="measurmentGoButton" onclick="measurementStartClick()">Go</button>
+        <button type="button" class="btn btn-success" id="measurmentNextButton" onclick="measurementNextClick() disabled">Następny punkt</button>`;
 }
 
 function auto3DModeClick() {
@@ -16,18 +18,24 @@ function auto3DModeClick() {
         <div id="measurements1Help" class="form-text pb-3">Pomiary będą rozmieszczone równomiernie na okręgu. Przedział 2-100</div>
         <label for="measurements2" class="form-label pt-3">Liczba pomiarów pionowych</label>
         <input type="number" class="form-control" id="measurements2" aria-describedby="measurements2Help" min="2" max="100">
-        <div id="measurements2Help" class="form-text">Pomiary rozmieszczone równo na przedziale (x, Y). Przedział 2-100 punktów</div>`;
+        <div id="measurements2Help" class="form-text">Pomiary rozmieszczone równo na przedziale (x, Y). Przedział 2-100 punktów</div>
+        <button type="button" class="btn btn-primary" id="measurmentGoButton" onclick="measurementStartClick()">Go</button>`;
 }
 
 function manualModeClick() {
     var measurementControlSection = document.getElementById("measurementControlSection");
-    measurementControlSection.innerHTML = "To be implemented or not to be implemented";
+    measurementControlSection.innerHTML = `
+        <label for="measurements1" class="form-label">Podaj kąt o który chcesz przesunąć ramię:</label>
+        <input type="number" class="form-control" id="measurements1" aria-describedby="measurements1Help" min="1" max="360">
+        <div id="measurements1Help" class="form-text pb-3">ruch zgodnie ze wskazówkami zegara</div>
+        <button type="button" class="btn btn-primary" id="measurmentGoButton" onclick="measurementStartClick()">Go</button>`;
 }
 
 function measurementStartClick(){
     if(isMoving == false){
         isMoving = true
         checkState()
+        document.querySelector('#measurmentGoButton').disabled = true
         $.get('_moveSteppers', {angle: document.getElementById('measurements1').value })
     }
 }
@@ -40,17 +48,20 @@ function checkState(){
           url: "/_checkState",
           type: "get",
           success: function(response) {
-            if(response == "True"){
+            if(response == "0"){
+                stateField.innerHTML = `<button type="button" class="btn btn-danger" style="pointer-events: none;" id="stateDanger">Błąd</button>`;
+            }else if(response == "1"){
                 stateField.innerHTML = `<button type="button" class="btn btn-success" style="pointer-events: none;" id="stateReady">Gotowy</button>`;
-                isMoving = true; 
+                isMoving = false; 
+                document.querySelector('#measurmentGoButton').disabled = false
                 clearInterval(interval)
-            }else{
+            }else if(response == "2"){
                 stateField.innerHTML = `
                 <button class="btn btn-primary" type="button" style="pointer-events: none;" id="stateMoving">
                 <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
                 Obracanie
                 </button>`;
-                isMoving = false;  
+                isMoving = true;  
             }
            },
           error: function(xhr) {
@@ -60,13 +71,3 @@ function checkState(){
     }, 1000);
 }
 
-/*
-$(function() {
-    $('a#process_input').bind('click', function() {
-      $.getJSON('/_moveSteppers', {
-        angle: $('input[id="measurements1"]').val(),
-      });
-      return false;
-    });
-  });
-*/
